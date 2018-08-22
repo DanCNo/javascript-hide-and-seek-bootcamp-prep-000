adce70d46668ec8decb6fbf3a207e3ef02b2d7b5
Cheol Noh <dan.noh@gmail.com>
2018-08-22 21:57:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ function increaseRankBy(n){
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   
   for (var i = 0; i < lis.length; i++) {
+    console.log(lis[i].innerHTML);
     lis[i].innerHTML = lis[i].innerHTML + n;
   }
 }
+
+increaseRankBy(5);
 
 function deepestChild(){
   return document.querySelector('#grand-node div div div div');
