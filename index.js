ffe9f966333aa6657ed6fc0297d2d0adb6cbaf9a
Cheol Noh <dan.noh@gmail.com>
2018-08-22 22:00:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('div div.target');
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (var i = 0; i < lis.length; i++) {
+    console.log(lis[i].innerHTML);
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + parseInt(n);
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+}
 
