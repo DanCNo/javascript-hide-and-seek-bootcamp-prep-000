71836db7e776877380e0c56586d4376dea394a3b
Cheol Noh <dan.noh@gmail.com>
2018-08-22 21:38:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.getElementByID('app').querySelector('div.nested div.target');
+  return document.getElementById('app').querySelector('div.nested div.target');
 }
 
 function increaseRankBy(n){
